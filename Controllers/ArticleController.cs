f23c7c27cada5ca077429fbe7a0dfc2aefe1f9e0
jack030 <hajebrahimi@aut.ac.ir>
2025-04-25 02:22:42 +0330

    article Controller

---
+++ Controllers/ArticleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WikiDafoos.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly DafoosDbContext _dafoosDbContext;
+
+        public ArticleController(DafoosDbContext dafoosDbContext)
+        {
+            _dafoosDbContext = dafoosDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var articles = await _dafoosDbContext.Articles.Include(a => a.Category).ToListAsync();
+            return View(articles);
+        }
+    }
+}
