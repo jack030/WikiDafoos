0cede7bfd6832183bbf2e10ceec8d62609c3ee63
jack030 <hajebrahimi@aut.ac.ir>
2025-04-25 02:25:28 +0330

    controller CRUD

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using WikiDafoos.Models;
 
 namespace WikiDafoos.Controllers
 {
@@ -16,6 +17,63 @@ namespace WikiDafoos.Controllers
         {
             var articles = await _dafoosDbContext.Articles.Include(a => a.Category).ToListAsync();
             return View(articles);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                _dafoosDbContext.Articles.Add(article);
+                await _dafoosDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(article);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var article = await _dafoosDbContext.Articles.FindAsync(id);
+            if (article == null) return NotFound();
+            return View(article);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                _dafoosDbContext.Update(article);
+                await _dafoosDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(article);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var article = await _dafoosDbContext.Articles.FindAsync(id);
+            return View(article);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var article = await _dafoosDbContext.Articles.FindAsync(id);
+            _dafoosDbContext.Articles.Remove(article);
+            await _dafoosDbContext.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var article = await _dafoosDbContext.Articles.Include(a => a.Category).FirstOrDefaultAsync(a => a.Id == id);
+            if (article == null) return NotFound();
+            return View(article);
         }
     }
 }
